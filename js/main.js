d89acfcca1130cb592042e074ed3349e7f78e24f
Gleb Ershov <gleb.ershov.04@icloud.com>
2023-12-03 05:54:26 +0300

    added system theme check

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
+//Проверека темной темы на уровне системных настроек 
+
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    document.body.classList.add('dark');
+}
 
 //Проверка темной темы в  localstorage
+
 if (localStorage.getItem('darkmode') === 'dark') {
     btnDarkMode.classList.toggle("dark-mode-btn--active");
     document.body.classList.add('dark');
-} else if (localStorage.getItem('darkmode') === 'light') {
-
 }
+
+//Если меняются системные настройки 
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event)=> {
+   const newColorScheme = event.matches ? 'dark' : 'light';
+
+   alert('Change!!!')
+   
+   if (newColorScheme === "dark") {
+        btnDarkMode.classList.toggle("dark-mode-btn--active");
+        document.body.classList.add('dark');
+   } else {
+        btnDarkMode.classList.remove("dark-mode-btn--active");
+        document.body.classList.remove('dark');
+   }
+})
+
 //Включение ночного режима по кнопке 
+
 btnDarkMode.addEventListener('click', () => {
     btnDarkMode.classList.toggle("dark-mode-btn--active");
     const isDark = document.body.classList.toggle("dark");
