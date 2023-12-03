ca1068269b5952c678aa884bf0df8cec1e0bf948
Gleb Ershov <gleb.ershov.04@icloud.com>
2023-12-03 04:03:00 +0300

    add local storage for darkmode

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
+
+//Проверка темной темы в  localstorage
+if (localStorage.getItem('darkmode') === 'dark') {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    document.body.classList.add('dark');
+} else if (localStorage.getItem('darkmode') === 'light') {
+
+}
+//Включение ночного режима по кнопке 
 btnDarkMode.addEventListener('click', () => {
     btnDarkMode.classList.toggle("dark-mode-btn--active");
-    document.body.classList.toggle("dark");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem('darkmode', 'dark')
+    } else {
+        localStorage.setItem('darkmode', 'light');
+    }
+
 })
